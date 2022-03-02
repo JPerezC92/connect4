5005220bb0969bd8256d9ae970c56cc40ed83e77
Philip Perez Castro <jperez.c92@gmail.com>
2022-03-01 23:05:21 -0500

    UPDATED - Board

--- src/Domain/Board.ts
+++ src/Domain/Board.ts
@@ -4,30 +4,30 @@ import { Token } from "./Token";
 
 export class Board {
   value: Token[][];
-  readonly maxAxisY: number = 0;
-  readonly maxAxisX: number = 0;
+  readonly rows: number = 0;
+  readonly columns: number = 0;
   public readonly _tokensInRowToWin: number = 4;
 
-  constructor(props: { value: Token[][]; maxAxisY: number; maxAxisX: number }) {
-    this.maxAxisX = props.maxAxisX;
-    this.maxAxisY = props.maxAxisY;
-
-    this.value = props.value.length > 0 ? props.value : this.createBoard();
+  public get maxAxisY(): number {
+    return this.rows - 1;
+  }
+  public get maxAxisX(): number {
+    return this.columns - 1;
   }
 
-  public Rows(): number {
-    return this.maxAxisY + 1;
-  }
+  constructor(props: { value: Token[][]; rows: number; columns: number }) {
+    const { value, rows, columns } = props;
+    this.columns = columns;
+    this.rows = rows;
 
-  public Columns(): number {
-    return this.maxAxisX + 1;
+    this.value = value.length > 0 ? value : this.createBoard({ columns, rows });
   }
 
   static createDefaultBoard(): Board {
     return new Board({
       value: [],
-      maxAxisY: 5,
-      maxAxisX: 6,
+      rows: 6,
+      columns: 7,
     });
   }
 
@@ -42,10 +42,13 @@ export class Board {
     return token && token[column];
   }
 
-  private createBoard(): Token[][] {
-    const rows = this.Rows();
-    const columns = this.Columns();
-
+  private createBoard({
+    columns,
+    rows,
+  }: {
+    columns: number;
+    rows: number;
+  }): Token[][] {
     const tokenMatrix: Token[][] = forInRange(rows).map((_, y): Token[] =>
       forInRange(columns).map(
         (_, x): Token => Token.create({ coords: Coords.new({ x, y }) })
@@ -57,7 +60,7 @@ export class Board {
 
   public getColumns(): Token[][] {
     const tokenList = this.value.flat();
-    const tokenColumnList: Token[][] = forInRange(this.Columns()).map(
+    const tokenColumnList: Token[][] = forInRange(this.maxAxisX).map(
       (_, x): Token[] => tokenList.filter((token) => token.coords.x === x)
     );
 
@@ -70,11 +73,11 @@ export class Board {
     let tokenDiagonalsList: Token[][] = [];
     const tokenList = this.value.flat();
 
-    const topLeftDiagonals = forInRange(this.Rows())
+    const topLeftDiagonals = forInRange(this.maxAxisY)
       .map((_, y) => {
         initialCoords = Coords.new({ x: 0, y });
 
-        return forInRange(this.Rows() - y)
+        return forInRange(this.maxAxisY - y)
           .map(() => {
             const diagonal = tokenList.filter((token) =>
               token.coords.isEqual(initialCoords)
@@ -86,11 +89,11 @@ export class Board {
       })
       .filter((diagonal) => diagonal.length > 0);
 
-    const topRigthDiagonals = forInRange(this.Columns())
+    const topRigthDiagonals = forInRange(this.maxAxisX)
       .map((_, x) => {
         initialCoords = Coords.new({ x: ++x, y: 0 });
 
-        return forInRange(this.Columns() - x)
+        return forInRange(this.maxAxisX - x)
           .map(() => {
             const diagonal = tokenList.filter((token) =>
               token.coords.isEqual(initialCoords)
@@ -102,11 +105,11 @@ export class Board {
       })
       .filter((diagonal) => diagonal.length > 0);
 
-    const topLeftDiagonalsInverse = forInRange(this.Rows())
+    const topLeftDiagonalsInverse = forInRange(this.maxAxisY)
       .map((_, y): Token[] => {
-        initialCoords = Coords.new({ x: this.maxAxisX, y });
+        initialCoords = Coords.new({ x: this.columns, y });
 
-        return forInRange(this.Rows() - y)
+        return forInRange(this.maxAxisY - y)
           .map((): Token[] => {
             const diagonal = tokenList.filter((token) =>
               token.coords.isEqual(initialCoords)
@@ -118,11 +121,11 @@ export class Board {
       })
       .filter((diagonal) => diagonal.length > 0);
 
-    const topRigthDiagonalsInverse = forInRange(this.Columns())
+    const topRigthDiagonalsInverse = forInRange(this.maxAxisX)
       .map((_, x): Token[] => {
         initialCoords = Coords.new({ x: x, y: 0 });
 
-        return forInRange(this.Rows())
+        return forInRange(this.maxAxisY)
           .map((): Token[] => {
             const diagonal = tokenList.filter((token) =>
               token.coords.isEqual(initialCoords)
